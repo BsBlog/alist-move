3e6d170dc2045060a6daab5cf74ec032a40159a5
Bnq Dzj <64721413+BnqDzj@users.noreply.github.com>
2024-08-24 12:57:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import requests
 import time
 import os
 
-# 配置
 base_url = os.environ["Alist_Base_Url"]
 username = os.environ["Alist_Username"]
 password = os.environ["Alist_Password"]
 
 
-# 获取Token
 def get_token(username, password):
     url = f"{base_url}/api/auth/login"
     data = {"username": username, "password": password}
@@ -17,7 +15,6 @@ def get_token(username, password):
     return response.json()["data"]["token"]
 
 
-# 列出目录中的子文件夹
 def list_folders(token, dir_path):
     url = f"{base_url}/api/fs/list"
     headers = {"Authorization": f"{token}"}
@@ -26,7 +23,6 @@ def list_folders(token, dir_path):
     response.raise_for_status()
     items = response.json()["data"]["content"]
 
-    # 如果items为None或为空，返回空列表
     if not items:
         return []
 
@@ -34,7 +30,6 @@ def list_folders(token, dir_path):
     return folders
 
 
-# 复制文件夹
 def copy_folders(token, src_dir, dst_dir, names):
     url = f"{base_url}/api/fs/copy"
     headers = {"Authorization": f"{token}"}
@@ -48,10 +43,9 @@ def copy_folders(token, src_dir, dst_dir, names):
     return response.json()
 
 
-# 获取未完成任务
 def get_pending_tasks(token):
     url = f"{base_url}/api/admin/task/copy/undone"
-    headers = {"Authorization": f"{token}"}  # 直接使用token
+    headers = {"Authorization": f"{token}"}
     response = requests.get(url, headers=headers)
 
     # 处理可能的JSONDecodeError
@@ -62,10 +56,9 @@ def get_pending_tasks(token):
     return data
 
 
-# 删除文件夹
 def delete_folder(token, dir_path, names):
     url = f"{base_url}/api/fs/remove"
-    headers = {"Authorization": f"{token}"}  # 直接使用token
+    headers = {"Authorization": f"{token}"}
     data = {
         "dir": dir_path,
         "names": names
@@ -75,33 +68,28 @@ def delete_folder(token, dir_path, names):
     return response.json()
 
 
-# 主函数
 def main():
     token = get_token(username, password)
 
-    # 获取/movies下的所有子文件夹
     movies_src_dir = os.environ["Movies_Path"]
     movies_dst_dir = os.environ["Target_Movies_Path"]
     movie_folders = list_folders(token, movies_src_dir)
     if movie_folders:
         copy_folders(token, movies_src_dir, movies_dst_dir, movie_folders)
 
-    # 获取/tv下的所有子文件夹
     tv_src_dir = os.environ["TV_Path"]
     tv_dst_dir = os.environ["Target_TV_Path"]
     tv_folders = list_folders(token, tv_src_dir)
     if tv_folders:
         copy_folders(token, tv_src_dir, tv_dst_dir, tv_folders)
+    time.sleep(3)
 
-    # 等待所有复制任务完成
     while True:
-        time.sleep(5)
         pending_tasks = get_pending_tasks(token)
         if not pending_tasks:
             break
-        time.sleep(5)  # 每5秒检查一次
+        time.sleep(1)
 
-    # 删除源文件夹
     if movie_folders:
         delete_folder(token, movies_src_dir, movie_folders)
     if tv_folders:
