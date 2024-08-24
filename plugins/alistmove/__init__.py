a59bc6534891aa440714a8fc2ad2c63b4d1ff9c3
Bnq Dzj <64721413+BnqDzj@users.noreply.github.com>
2024-08-24 16:48:12 +0800

    Update __init__.py

--- plugins/alistmove/__init__.py
+++ plugins/alistmove/__init__.py
@@ -21,7 +21,7 @@ class AlistMove(_PluginBase):
     # 作者主页
     author_url = "None"
     # 插件配置项ID前缀
-    plugin_config_prefix = "Alist_Move_"
+    plugin_config_prefix = "alistmove_"
     # 加载顺序
     plugin_order = 1
     # 可使用的用户级别
